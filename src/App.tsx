4645cf342aae89d453ffb173313d1bb66f86096f
Kyrylo Savieliev <k_savieliev@icloud.com>
2025-05-21 14:12:02 +0200

    feat: add basic seo (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { router } from "./router"
 import * as TanStackQueryProvider from './integrations/tanstack-query/root-provider.tsx'
 import { RouterProvider } from "@tanstack/react-router"
 import { useAuthStore } from "./features/auth/stores"
+import { createHead, UnheadProvider } from '@unhead/react/client'
 
 export const InnerApp = () => {
   const auth = useAuthStore()
+  const head = createHead()
 
   return (
     <TanStackQueryProvider.Provider>
-      <RouterProvider router={router} context={{ auth }} />
+      <UnheadProvider head={head}>
+        <RouterProvider router={router} context={{ auth }} />
+      </UnheadProvider>
     </TanStackQueryProvider.Provider>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
@@ -9,7 +10,12 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
+    plugins: [
+      TanStackRouterVite({ autoCodeSplitting: true }),
+      viteReact(),
+      tailwindcss(),
+      svgr()
+    ],
     base: '/',
     test: {
       globals: true,

398030f905ee82db2d1523c8995ca2dd095e37f3
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2025-05-29 17:39:35 +0300

    fix: fix anchor links in navbar

--- src/core/components/promo-page/navbar.tsx
+++ src/core/components/promo-page/navbar.tsx
@@ -10,22 +10,31 @@ export const PromoNavbar: React.FC = () => {
   const { t } = useTranslation()
 
   const links = [
-    t('promo.sections.features'),
-    t('promo.sections.pricing'),
-    t('promo.sections.newsletter')
+    {
+      name:t('promo.sections.features'),
+      href: '#features'
+    },
+    {
+      name: t('promo.sections.pricing'),
+      href: '#pricing'
+    },
+    {
+      name: t('promo.sections.newsletter'),
+      href: '#newsletter'
+    }
   ]
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-8 2xl:max-w-[1400px] grid grid-cols-3 h-16 items-center">
-        <Link to="/" className="flex items-center justify-start gap-2">
+        <a href="/#hero" className="flex items-center justify-start gap-2">
           <Logo className="size-8" />
           <span className="text-xl font-bold max-lg:hidden">GatherFlow</span>
-        </Link>
+        </a>
         <nav className="hidden md:flex items-center justify-center gap-6">
-          {links.map((link, index) => (
-            <a key={index} href={`#${link.toLowerCase()}`} className="text-sm font-medium hover:text-primary">
-              {link}
+          {links.map(({ name, href }, index) => (
+            <a key={index} href={href} className="text-sm font-medium hover:text-primary">
+              {name}
             </a>
           ))}
         </nav>
